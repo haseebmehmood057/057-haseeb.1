458a6f767a278c4ab5bba57d9a495c41bd2b6935
hasee <hasee@DESKTOP-6495UOO>
2022-01-12 00:50:12 +0500

    Add project files.

---
+++ DataDb/StoreDb.cs
@@ -0,0 +1,18 @@
+﻿using _057_haseeb._1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _057_haseeb._1.DataDb
+{
+    public class StoreDb : DbContext
+    {
+        public StoreDb(DbContextOptions<StoreDb> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderModel> orders { get; set; }
+    }
+}
